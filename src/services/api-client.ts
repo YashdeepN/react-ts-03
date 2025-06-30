c6f538b96b07aae8dc295bb01e84fbc588cd49ce
YashdeepN <yashdeepneema@hotmail.com>
2025-06-30 01:36:30 -0500

    handle canceling in custom hook using AbortControl

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: "b343bdd47f3c439ebcc4344845358789",
+    key: "1d0aa4ca2552471caa23dda3748e86f2",
   },
 });
