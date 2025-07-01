d20bc459ecf33f778c0b6a116c779c0037256049
YashdeepN <yashdeepneema@hotmail.com>
2025-07-01 07:21:15 -0500

    show genres in aside and set templateColumns

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -8,14 +8,14 @@ const GenreList = () => {
     <>
       <List>
         {data.map((genre) => (
-          <ListItem key={genre.id}>
+          <ListItem key={genre.id} paddingY={1}>
             <HStack>
               <Image
                 boxSize="32px"
                 borderRadius={8}
                 src={getCroppedImageUrl(genre.image_background)}
               />
-              <Text>{genre.name}</Text>
+              <Text fontSize="lg">{genre.name}</Text>
             </HStack>
           </ListItem>
         ))}
