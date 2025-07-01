c56c86754919664efc80663c9e10afd8441164cf
YashdeepN <yashdeepneema@hotmail.com>
2025-06-30 23:27:15 -0500

    displaying platform icons using slug

--- src/components/PlatformIconList.tsx
+++ src/components/PlatformIconList.tsx
@@ -1,17 +1,44 @@
-import React from "react";
+import {
+  FaWindows,
+  FaPlaystation,
+  FaXbox,
+  FaApple,
+  FaLinux,
+  FaAndroid,
+} from "react-icons/fa";
+
+import { MdPhoneIphone } from "react-icons/md";
+
+import { SiNintendo } from "react-icons/si";
+import { BsGlobe } from "react-icons/bs";
+
 import type { Platform } from "../hooks/useGames";
-import { Text } from "@chakra-ui/react";
+import { HStack, Icon } from "@chakra-ui/react";
+import type { IconType } from "react-icons";
 
 interface Props {
   platforms: Platform[];
 }
 
 const PlatformIconList = ({ platforms }: Props) => {
+  const iconMap: { [key: string]: IconType } = {
+    xbox: FaXbox,
+    pc: FaWindows,
+    playstation: FaPlaystation,
+    nintendo: SiNintendo,
+    mac: FaApple,
+    linux: FaLinux,
+    ios: MdPhoneIphone,
+    web: BsGlobe,
+    android: FaAndroid,
+  };
   return (
     <>
-      {platforms.map((platform) => (
-        <Text key={platform.id}>{platform.slug}</Text>
-      ))}
+      <HStack marginY={1}>
+        {platforms.map((platform) => (
+          <Icon color="gray.500" as={iconMap[platform.slug]} />
+        ))}
+      </HStack>
     </>
   );
 };
