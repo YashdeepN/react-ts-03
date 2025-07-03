9e92ce4f6432627a7d21f670c3c988e36fa0d65a
YashdeepN <yashdeepneema@hotmail.com>
2025-07-03 00:34:14 -0500

    fix: fetch parent platforms instead of platforms.

--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -4,7 +4,9 @@ import { BsChevronDown } from "react-icons/bs";
 import usePlatforms from "../hooks/usePlatforms";
 
 const PlatformSelector = () => {
-  const { data, error, isLoading } = usePlatforms();
+  const { data, error } = usePlatforms();
+
+  if (error) return null;
   return (
     <Menu>
       <MenuButton as={Button} rightIcon={<BsChevronDown />}>
