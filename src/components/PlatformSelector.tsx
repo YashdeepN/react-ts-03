17b4968bd15c483552eaf396a9f38a1a8b8335ef
YashdeepN <yashdeepneema@hotmail.com>
2025-07-10 07:45:37 -0500

    create look up hooks

--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -1,6 +1,7 @@
 import { Button, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
 import { BsChevronDown } from "react-icons/bs";
 import usePlatforms, { type Platform } from "../hooks/usePlatforms";
+import usePlatform from "../hooks/usePlatform";
 
 interface Props {
   selectedPlatformId?: number;
@@ -10,9 +11,10 @@ interface Props {
 const PlatformSelector = ({ selectedPlatformId, onSelectPlatform }: Props) => {
   const { data, error } = usePlatforms();
 
-  const selectedPlatform = data?.results.find(
-    (p) => p.id === selectedPlatformId
-  );
+  const selectedPlatform = usePlatform(selectedPlatformId);
+  // data?.results.find(
+  // (p) => p.id === selectedPlatformId
+  // );
 
   if (error) return null;
   return (
--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,9 +1,10 @@
 // import useData from "./useData";
 
 import { useQuery } from "@tanstack/react-query";
-import genres from "../data/genres";
+// import genres from "../data/genres";
 
 import APIClient, { type FetchResponse } from "../services/api-client";
+import genres from "../data/genres";
 
 const apiClient = new APIClient<Genre>("/genres");
 
@@ -23,7 +24,7 @@ const useGenres = () => {
     queryKey: ["genres"],
     queryFn: apiClient.getAll,
     staleTime: 24 * 60 * 60 * 1000, // 24 hours
-    initialData: { count: genres.length, results: genres, next: "" },
+    initialData: genres,
   });
 };
 
