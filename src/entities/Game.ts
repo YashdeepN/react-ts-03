c7a49293deeac7cbaba9b94b2d74fad48ca2eb80
YashdeepN <yashdeepneema@hotmail.com>
2025-07-15 23:33:46 -0500

    build game attributes

--- src/entities/Game.ts
+++ src/entities/Game.ts
@@ -1,9 +1,13 @@
+import type { Genre } from "./Genre";
 import type { Platform } from "./Platform";
+import type { publisher } from "./Publisher";
 
 export interface Game {
   id: number;
   slug: string;
   name: string;
+  genres: Genre[];
+  publishers: publisher[];
   description_raw: string;
   background_image: string;
   parent_platforms: { platform: Platform }[];
